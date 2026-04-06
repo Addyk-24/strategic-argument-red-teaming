ef1950cd0c89255abe9fddc21c2d57f627d63eab
Addyk-24 <addykatkar24@gmail.com>
2026-04-06 17:55:57 +0530

    fix: - server/app.py main() function not callable (missing if __name__ == '__main__')

--- server/app.py
+++ server/app.py
@@ -447,5 +447,8 @@ async def state() -> dict[str, Any]:
         "info": {}
     }
 
-if __name__ == "__main__":
+def main() -> None:
     uvicorn.run(app, host="0.0.0.0", port=7860)
+
+if __name__ == "__main__":
+    main()
