89515479a758f97bda223ce1b5a266a197ebefce
Addyk-24 <addykatkar24@gmail.com>
2026-04-06 15:34:06 +0530

    updated gradio ui

--- app.py
+++ app.py
@@ -201,8 +201,7 @@ with gr.Blocks(theme=corporate_dark_theme, css=custom_css) as demo:
             chatbot = gr.Chatbot(
                 show_label=False,
                 height=400,
-                bubble_full_width=False,
-                show_copy_button=True
+                elem_classes="message-value",
             )
 
     # --- Event Listeners ---
